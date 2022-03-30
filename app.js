971ec348b840c3cefb79b1b78728c679ec3686dc
Kouacou <24gkouacou@FVFY36M9J1WK.tld>
2022-03-30 10:26:08 -0500

    3/30/22

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 
 
 
+function n(){
+    var q = prompt("would you like to draw to draw a number? if yes press y.")
 
-function Grace() {
-    alert ("at the end")
+        
+}
+n()
+
+function grace(){ 
+    do{
+        var CPRan = Math.floor(Math.random()* 10 ) + 1;
+            alert(`The computer will now draw a card.`);
+         
+            var CPTotal = CPTotal + CPRan
+    
+            alert(`The computer have a total of ${CPTotal}`);
+    
+            
+        }while (CPTotal < 16);
 }
 
+grace
 
-function program(){
-    Grace()
+function human () { 
+    do{
+        var USRan = Math.floor(Math.random()* 10 ) + 1;
 
+        var USTotal = USTotal + USRan
+        alert(`human number is ${USRan}`)
+        alert(`The human has a total of ${USTotal}`);
 
-
-
-
-    var q = prompt("fi you want to play press b.")
-    if (q == "b") {
-        program() // recursive function
-    }else{
-        alert("Thank you for playing")
+        if (USTotal > 20) {
+            break;
         }
         
+         var desicioin = prompt(`human do you want to another card? if yes press y`)
+        
+    } while ((USTotal < 20 ) && (desicioin == "y"));
+}
+    
+human()
+    
+function results() {
+    do{
+    if ( ( CPTotal < USTotal) && ( CPTotal <= 20) && (USTotal <= 20) ){
+        alert(`You have won`);
+    }else if ( (CPTotal > USTotal ) && ( CPTotal <= 20) && ( USTotal <=20) ){
+    alert( `the computer has won`);
+    } else if ( ( CPTotal = USTotal) && ( CPTotal <= 20) && ( USTotal <= 20) ) {
+        alert(` you've tied with the computer `);
     }
+    alert(`The computer as a total of ${SPTotal}\n\n\n You have a total of ${USTotal} `)
+    var q2 = (`do you want to play again? Press y to play another game`)
 
+    }while (q2 == "y")
+}
+
+results()
+    
+    // if (q == "y") {
+    //     program() // recursive function
+    // }else{
+    //     alert("Thank you for playing")
+    //     }    
